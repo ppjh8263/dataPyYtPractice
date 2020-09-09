9bf7590308aa3a4f3502f4d51a848cbf0d38d9df
JinHyung <ppjh8263@dnx.kr>
2020-09-09 18:07:55 +0900

    namu wiki

--- namuwikiPractice.py
+++ namuwikiPractice.py
@@ -1,5 +1,6 @@
-import requests
+import requests #크롤링 라이브러리
 from bs4 import BeautifulSoup
+import pandas as pd
 import re
 
 # 크롤링할 사이트 주소를 정의합니다.
@@ -13,5 +14,78 @@ contents_table = soup.find(name="table")
 table_body = contents_table.find(name="tbody")
 table_rows = table_body.find_all(name="tr")
 
+# a태그의 href 속성을 리스트로 추출하여, 크롤링 할 페이지 리스트를 생성합니다.
 page_url_base = "https://namu.wiki"
 page_urls = []
+for index in range(0, len(table_rows)):
+    first_td = table_rows[index].find_all('td')[0]
+    td_url = first_td.find_all('a')
+    if len(td_url) > 0:
+        page_url = page_url_base + td_url[0].get('href')
+        if 'png' not in page_url:
+            page_urls.append(page_url)
+
+# 중복 url을 제거합니다.
+page_urls = list(set(page_urls))
+for page in page_urls[:5]:
+    print(page)
+
+# 크롤링한 데이터를 데이터 프레임으로 만들기 위해 준비합니다.
+columns = ['title', 'category', 'content_text']
+df = pd.DataFrame(columns=columns)
+
+# 각 페이지별 '제목', '카테고리', '본문' 정보를 데이터 프레임으로 만듭니다.
+for page_url in page_urls:
+
+    # 사이트의 html 구조에 기반하여 크롤링을 수행합니다.
+    req = requests.get(page_url)
+    html = req.content
+    soup = BeautifulSoup(html, 'lxml')
+    contents_table = soup.find(name="article")
+    title = contents_table.find_all('h1')[0]
+
+    # 카테고리 정보가 없는 경우를 확인합니다.
+    if len(contents_table.find_all('ul')) > 0:
+        category = contents_table.find_all('ul')[0]
+    else:
+        category = None
+
+    content_paragraphs = contents_table.find_all(name="div", attrs={"class": "wiki-paragraph"})
+    content_corpus_list = []
+
+    # 페이지 내 제목 정보에서 개행 문자를 제거한 뒤 추출합니다. 만약 없는 경우, 빈 문자열로 대체합니다.
+    if title is not None:
+        row_title = title.text.replace("\n", " ")
+    else:
+        row_title = ""
+
+    # 페이지 내 본문 정보에서 개행 문자를 제거한 뒤 추출합니다. 만약 없는 경우, 빈 문자열로 대체합니다.
+    if content_paragraphs is not None:
+        for paragraphs in content_paragraphs:
+            if paragraphs is not None:
+                content_corpus_list.append(paragraphs.text.replace("\n", " "))
+            else:
+                content_corpus_list.append("")
+    else:
+        content_corpus_list.append("")
+
+    # 페이지 내 카테고리정보에서 “분류”라는 단어와 개행 문자를 제거한 뒤 추출합니다. 만약 없는 경우, 빈 문자열로 대체합니다.
+    if category is not None:
+        row_category = category.text.replace("\n", " ")
+    else:
+        row_category = ""
+
+    # 모든 정보를 하나의 데이터 프레임에 저장합니다.
+    row = [row_title, row_category, "".join(content_corpus_list)]
+    series = pd.Series(row, index=df.columns)
+    df = df.append(series, ignore_index=True)
+
+
+def text_cleaning(text):
+    hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')  # 한글의 정규표현식을 나타냅니다.
+    result = hangul.sub('', text)
+    return result
+
+df['title'] = df['title'].apply(lambda x: text_cleaning(x))
+df['category'] = df['category'].apply(lambda x: text_cleaning(x))
+df['content_text'] = df['content_text'].apply(lambda x: text_cleaning(x))
